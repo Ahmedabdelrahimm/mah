125bd23cc81db892b7b2aa29afae1d60f8ec5fc0
Ahmed Abdelrahim <155884920+Ahmedabdelrahimm@users.noreply.github.com>
2024-04-27 14:24:44 +0300

    Add files via upload
    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,125 @@
+function showModal() {
+    document.getElementById("modal").style.visibility = "visible";
+    document.getElementById("modal").style.opacity = "1";
+    document.getElementById("main").style.filter = "blur(8px)";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.opacity = "0";
+    document.getElementById("modal").style.visibility = "hidden";
+    document.getElementById("main").style.filter = "blur(0px)";
+}
+
+function showModal2() {
+    document.getElementById("modal2").style.visibility = "visible";
+    document.getElementById("modal2").style.opacity = "1";
+    document.getElementById("main2").style.filter = "blur(8px)";
+}
+
+function closeModal2() {
+    document.getElementById("modal2").style.opacity = "0";
+    document.getElementById("modal2").style.visibility = "hidden";
+    document.getElementById("main2").style.filter = "blur(0px)";
+}
+
+let slideIndex = 1;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3500);
+}
+
+document.getElementById("new-collection-button").onclick = function () {
+    window.location.href = "shop.html";
+};
+document.getElementById("register-button").onclick = function () {
+    window.location.href = "register.html";
+};
+
+
+function showDescription(card) {
+    card.getElementsByClassName('description')[0].style.display = 'block';
+}
+
+function hideDescription(card) {
+    card.getElementsByClassName('description')[0].style.display = 'none';
+}
+
+
+const form = document.querySelector("#credit-card");
+
+const cardNumber = document.querySelector("#card-number");
+const cardHolder = document.querySelector("#name-text");
+const cardExpiration = document.querySelector("#valid-thru-text");
+const cardCVV = document.querySelector("#cvv-text");
+
+const cardNumberText = document.querySelector(".number-vl");
+const cardHolderText = document.querySelector(".name-vl");
+const cardExpirationText = document.querySelector(".expiration-vl");
+const cardCVVText = document.querySelector(".cvv-vl");
+
+cardNumber.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardNumberText.innerText = "1234 5678 9101 1121";
+    } else {
+        const valuesOfInput = e.target.value.replaceAll(" ", "");
+
+        if (e.target.value.length > 14) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+            cardNumberText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+        } else if (e.target.value.length > 9) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+            cardNumberText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+        } else if (e.target.value.length > 4) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+            cardNumberText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+        } else {
+            cardNumberText.innerHTML = valuesOfInput
+        }
+    }
+})
+
+cardHolder.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardHolderText.innerHTML = "NOAH JACOB";
+    } else {
+        cardHolderText.innerHTML = e.target.value.toUpperCase();
+    }
+})
+
+cardExpiration.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardExpirationText.innerHTML = "02/40";
+    } else {
+        const valuesOfInput = e.target.value.replace("/", "");
+
+        if (e.target.value.length > 2) {
+            e.target.value = valuesOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+            cardExpirationText.innerHTML = valuesOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+        } else {
+            cardExpirationText.innerHTML = valuesOfInput;
+        }
+    }
+})
+
+cardCVV.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardCVVText.innerHTML = "123";
+    } else {
+        cardCVVText.innerHTML = e.target.value;
+    }
+})
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    alert("Credit Card Added!");
+})
